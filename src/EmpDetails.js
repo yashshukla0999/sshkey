f7d75b50c62a57787a5900a0a66363129ece2dd6
yashshukla099 <yashshukla0999@gmail.com>
2023-12-01 12:35:42 +0530

    git commit -m "first commit"

---
+++ src/EmpDetails.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+
+const EmpDetail = () => {
+    const { empid } = useParams();
+
+    const [empdata, empdatachange] = useState({});
+
+    useEffect(() => {
+        // Fetch a single employee by ID from Spring Boot endpoint
+        fetch(`http://localhost:8080/employee/${empid}`)
+            .then((res) => res.json())
+            .then((data) => empdatachange(data))
+            .catch((error) => console.error("Error fetching employee:", error));
+    }, [empid]);
+
+    return (
+        <div>
+            {/* <div className="row">
+                <div className="offset-lg-3 col-lg-6"> */}
+
+               <div className="container">
+                
+            <div className="card row" style={{ "textAlign": "left" }}>
+                <div className="card-title">
+                    <h2>Employee Create</h2>
+                </div>
+                <div className="card-body"></div>
+
+                {empdata &&
+                    <div>
+                        <h2>The Employee name is : <b>{empdata.name}</b>  ({empdata.id})</h2>
+                        <h3>Contact Details</h3>
+                        <h5>Email is : {empdata.email}</h5>
+                        <h5>Phone is : {empdata.phone}</h5>
+                        <Link className="btn btn-danger" to="/">Back to Listing</Link>
+                    </div>
+                }
+            </div>
+            </div>
+            {/* </div>
+            </div> */}
+        </div >
+    );
+}
+
+export default EmpDetail;
---
+++ src/EmpEdit.js
@@ -0,0 +1,98 @@
+// EmpListing.js
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmpListing = () => {
+    // State to hold employee data
+    const [empdata, empdatachange] = useState(null);
+
+    // React hook to handle navigation
+    const navigate = useNavigate();
+
+    // Function to load employee details page
+    const LoadDetail = (id) => {
+        navigate("/employee/detail/" + id);
+    };
+
+    // Function to load employee edit page
+    const LoadEdit = (id) => {
+        navigate("/employee/edit/" + id);
+    };
+
+    // Function to handle employee removal
+    const Removefunction = (id) => {
+        if (window.confirm('Do you want to remove?')) {
+            // Send DELETE request to remove employee
+            fetch(`http://localhost:8080/employee/${id}`, {
+                method: "DELETE"
+            })
+            .then((res) => {
+                alert('Removed successfully.');
+                // Reload employee data after deletion
+                window.location.reload();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+        }
+    };
+
+    // UseEffect hook to fetch employee data on component mount
+    useEffect(() => {
+        fetch("http://localhost:8080/employee")
+            .then((res) => res.json())
+            .then((resp) => {
+                empdatachange(resp);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2>Employee Listing</h2>
+                </div>
+                <div className="card-body">
+                    <div className="divbtn">
+                        {/* Link to the create employee page */}
+                        <Link to="employee/create" className="btn btn-success">Add New (+)</Link>
+                    </div>
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                            <tr>
+                                <td>ID</td>
+                                <td>Name</td>
+                                <td>Email</td>
+                                <td>Phone</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {empdata &&
+                                empdata.map(item => (
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.phone}</td>
+                                        <td>
+    <button type="button" onClick={() => LoadEdit(item.id)} className="btn btn-success">Edit</button>
+    <button type="button" onClick={() => Removefunction(item.id)} className="btn btn-danger">Remove</button>
+    <button type="button" onClick={() => LoadDetail(item.id)} className="btn btn-primary">Details</button>
+</td>
+
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EmpListing;
