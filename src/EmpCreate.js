630e58773ce267798bb4f3ee283ef48cd25bdb87
Yash <yashshukla0999@gmail.com>
2023-12-15 18:44:48 +0530

    ff

--- src/EmpCreate.js
+++ src/EmpCreate.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
  
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://localhost:8080";
 const EmpCreate = () => {
 
     const[id,]=useState("");
--- src/EmpDetails.js
+++ src/EmpDetails.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://localhost:8080";
 
 
 const EmpDetail = () => {
